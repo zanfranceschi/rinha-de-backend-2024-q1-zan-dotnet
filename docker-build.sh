7dcf1cb7eaac60f1c0c5eeeeaed18cc224113ed6
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2024-02-19 22:18:01 -0300

    ..

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
 tag=$(date +"%Y%m%d%H%M")
+
 docker build -t rinha-2024q1-crebito-dotnet:$tag -t zanfranceschi/rinha-2024q1-crebito-dotnet:$tag .
 docker push zanfranceschi/rinha-2024q1-crebito-dotnet:$tag
 
