797ec89e85590b970320c218c6c028acd6784419
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2024-02-19 22:17:31 -0300

    tag nas imagens e multiplexing no postgres

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,5 @@
-docker build -t rinha-2024q1-crebito-dotnet -t zanfranceschi/rinha-2024q1-crebito-dotnet .
-docker push zanfranceschi/rinha-2024q1-crebito-dotnet
+tag=$(date +"%Y%m%d%H%M")
+docker build -t rinha-2024q1-crebito-dotnet:$tag -t zanfranceschi/rinha-2024q1-crebito-dotnet:$tag .
+docker push zanfranceschi/rinha-2024q1-crebito-dotnet:$tag
+
+echo $tag
